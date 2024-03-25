899046c569c21fb49f2ea7a51f38d100242b6ef0
Marcus Kim <marcuskim1989@gmail.com>
2024-03-25 11:29:06 -0700

    Finish creating props for ProductListItem

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -17,23 +17,3 @@ export default function MenuScreen() {
     );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        backgroundColor: 'white',
-        padding: 10,
-        borderRadius: 10
-    },
-    image: {
-        width: '100%',
-        aspectRatio: 1,
-    },
-    title: {
-        fontSize: 18,
-        fontWeight: '600',
-        marginVertical: 10,
-    },
-    price: {
-        color: Colors.light.tint,
-        fontWeight: 'bold',
-    },
-});
