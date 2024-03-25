ec4097d58fc558e338e43b8a2a0dd633cd70ca5b
Marcus Kim <marcuskim1989@gmail.com>
2024-03-25 09:51:59 -0700

    fix imports

--- src/app/components/ProductListItem.tsx
+++ src/app/components/ProductListItem.tsx
@@ -1,28 +1,31 @@
 import { StyleSheet, Text, View, Image } from 'react-native';
 import Colors from '../constants/Colors';
-import products from '../../../assets/data/products';
-import React from 'react';
+import {Product} from '../types'
 
-const product = products[0];
+const defaultPizzaImage = 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png'
 
-export const defaultImage =
-    'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png';
+type ProductListItemProps = {
+    product: Product
+}
 
-export default function MenuScreen() {
+const ProductListItem = ({ product }: ProductListItemProps) => {
     return (
         <View style={styles.container}>
-            <Image
-                source={{ uri: product.image || defaultImage }}
-                style={styles.image}
-            ></Image>
+            <Image source={{ uri: product.image || defaultPizzaImage}} style={styles.image}></Image>
             <Text style={styles.title}>{product.name}</Text>
             <Text style={styles.price}>${product.price}</Text>
         </View>
     );
-}
+};
+
+export default ProductListItem;
 
 const styles = StyleSheet.create({
-    container: {},
+    container: {
+        backgroundColor: 'white',
+        padding: 10,
+        borderRadius: 20,
+    },
     image: {
         width: '100%',
         aspectRatio: 1,
