02cd7f01a6b0e7a7036b203140adf7a1a5e545b2
Marcus Kim <marcuskim1989@gmail.com>
2024-03-25 14:10:10 -0700

    Create navigation to Product page

--- src/app/components/ProductListItem.tsx
+++ src/app/components/ProductListItem.tsx
@@ -1,7 +1,8 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Pressable } from 'react-native';
 import Colors from '../constants/Colors';
 import { Product } from '../types';
 import React from 'react';
+import {Link} from 'expo-router'
 
 type ProductListItemProps = {
     product: Product;
@@ -9,15 +10,17 @@ type ProductListItemProps = {
 
 const ProductListItem = ({ product }: ProductListItemProps) => {
     return (
-        <View style={styles.container}>
-            <Image
-                source={{ uri: product.image || defaultImage }}
-                style={styles.image}
-                resizeMode="contain"
-            />
+        <Link href={'/product'} asChild>
+            <Pressable style={styles.container}>
+                <Image
+                    source={{ uri: product.image || defaultImage }}
+                    style={styles.image}
+                    resizeMode="contain"
+                />
             <Text style={styles.title}>{product.name}</Text>
             <Text style={styles.price}>${product.price}</Text>
-        </View>
+        </Pressable>
+        </Link>
     );
 };
 
