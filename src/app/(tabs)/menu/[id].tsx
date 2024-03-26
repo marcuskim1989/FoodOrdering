a5a41f48c3497419a5516aba7a25cf1a33ff81f9
Marcus Kim <marcuskim1989@gmail.com>
2024-03-25 18:21:24 -0700

    Fix stack nav titles

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,12 +1,15 @@
 import { useLocalSearchParams } from 'expo-router';
 import React from 'react';
 import {View, Text} from 'react-native';
+import {Stack} from 'expo-router';
 
 const ProductDetailsScreen = () => {
     const {id} = useLocalSearchParams();
 
     return (
         <View>
+
+            <Stack.Screen options={{title: 'Details: ' + id}} />
             <Text style={{fontSize: 20}}>ProductDetailsScreen for id: {id}</Text>
         </View>
     )
--- src/app/(tabs)/menu/_layout.tsx
+++ src/app/(tabs)/menu/_layout.tsx
@@ -2,6 +2,10 @@ import {Stack} from 'expo-router'
 import React from 'react'
 
 export default function MenuStack() {
-    return <Stack />
+    return (
+        <Stack> 
+            <Stack.Screen name="index" options={{title: 'Menu'}} />
+        </Stack>
+    )
 }
 
