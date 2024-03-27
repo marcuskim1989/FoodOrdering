9380ec21a3f7094809f90184ab1a48c86646a419
Marcus Kim <marcuskim1989@gmail.com>
2024-03-26 22:30:48 -0700

    Begin creating shopping cart modal

--- src/app/(tabs)/menu/_layout.tsx
+++ src/app/(tabs)/menu/_layout.tsx
@@ -1,9 +1,33 @@
-import {Stack} from 'expo-router'
+import {Stack, Link} from 'expo-router'
 import React from 'react'
+import {Pressable} from 'react-native'
+import { FontAwesome } from '@expo/vector-icons'
+import Colors from '../../constants/Colors'
+
 
 export default function MenuStack() {
     return (
-        <Stack> 
+        <Stack screenOptions={{
+            headerRight: () => (
+                <Link href="/cart" asChild>
+                    <Pressable>
+                        {({ pressed }) => (
+                            <FontAwesome
+                                name="shopping-cart"
+                                size={25}
+                                color={
+                                    Colors.light.tint
+                                }
+                                style={{
+                                    marginRight: 15,
+                                    opacity: pressed ? 0.5 : 1,
+                                }}
+                            />
+                        )}
+                    </Pressable>
+                </Link>
+            ),
+        }}> 
             <Stack.Screen name="index" options={{title: 'Menu'}} />
         </Stack>
     )
